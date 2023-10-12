c489e212be62820ea3aa34059ff29dc78c32ab6d
tjahjoe <ntell634@gmail.com>
2023-10-13 06:01:08 +0700

    next menyesuaikan nilai

--- karyaku/main2.java
+++ karyaku/main2.java
@@ -11,17 +11,17 @@ public class main2 {
         String back = "";
         int n = 0, j, c;
 
-        String mhs[][] = new String[10][3];
+//menyesuaikan nilai
         String bio[] = { "Nama", "NIM", "kelas" };
         String mhsn[][] = { { "Wahyu", "1234455", "1C" }, { "Rizky", "1122334", "1C" }, { "Cahyana", "1234466", "1C" },
                 { "Dizky", "1177334", "1C" } };
-        int nilai[] = { 100, 98, 100, 89, 100, 0, 0, 0, 0, 0 };
+        int nilai[] = { 100, 98, 100, 89, 100, 0, 0, 0, 0, 0, 0, 0 };
         String ab[] = new String[3];
         do {
-            // System.out.println("Bayak siswa");
-            // int d = scInt.nextInt();
-            String aa[][] = new String[10][3];
-            //mhs = aa;
+            System.out.println("Bayak siswa");
+            int d = scInt.nextInt();
+            String aa[][] = new String[d][3];
+            String mhs[][] = new String[d][3];
 
             System.out.println(mhs.length);
             for (int x = 0; x < mhs.length; x++) {
@@ -51,13 +51,6 @@ public class main2 {
                     mhs[n][j] = aa[n][j];
                 }
                 System.out.println(aa[4][0]);
-                // for (int q = 0; q < mhs.length; q++) {
-                //     System.out.printf("%-3d|.\t|", q + 1);
-                //     for (int t = 0; t < bio.length; t++) {
-                //         System.out.printf(" %-6s|%-10s|", bio[t], mhs[q][t]);
-                //     }
-                //     System.out.printf("\t%d\n", nilai[q]);
-                // }
                 for (int a = 0; a < mhs.length; a++) {
                     for (int b = 0; b < mhs.length; b++) {
                         if (nilai[a] > nilai[b]) {
@@ -93,31 +86,12 @@ public class main2 {
                     }
                     System.out.printf("\t%d\n", nilai[q]);
                 }System.out.println(aa[4][0]);
-                // for (int s = 0; s < mhs.length; s++) {
-                // System.out.printf("%d.\t: %s\n", s + 1, mhs[s][0]);
-                // }
-                // System.out.println("Masukkan angka");
-                // n = scInt.nextInt();
-                // n -= 1;
-                // for (j = 0; j < bio.length; j++) {
-                // System.out.printf("Masukkan %s : ", bio[j]);
-                // mhs[n][j] = scStr.nextLine();
-                // }
-                // System.out.println(nilai[6]);
-                // for (int q = 0 ; q < mhs.length; q++){
-                // System.out.printf("%-3d|.\t|", q+1);
-                // for(int t = 0 ; t < bio.length; t++){
-                // System.out.printf(" %-6s|%-10s|", bio[t], mhs[q][t]);
-                // }
-                // System.out.printf("\t%d\n", nilai[q]);
-                // }
-
+                for (int s = 0; s < mhs.length; s++) {
+                    System.out.printf("%d.\t: %s\n", s + 1, aa[s][0]);
+                }
                 System.out.println("ketik y");
                 back = scStr.nextLine();
             } while (back.equalsIgnoreCase("y"));
         } while (back.equalsIgnoreCase("a"));
     }
 }
-// {"wahyu", "123", "1c"},
-// {"", "", ""},
-// {"", "", ""},
