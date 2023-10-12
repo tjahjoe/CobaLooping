a07ca13440237b98b5652e5649c0061f0b88b42d
tjahjoe <ntell634@gmail.com>
2023-10-12 14:03:13 +0700

    perurutan

--- karyaku/main2.java
+++ karyaku/main2.java
@@ -1,23 +1,21 @@
 // packagekaryaku;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class main2 {
     public static void main(String[] args) {
         Scanner scStr = new Scanner(System.in);
         Scanner scInt = new Scanner(System.in);
-        String back;
-        int n, j;
-        // String mhs[][] = new String[5][3];
-        // mhs [0][0] = "wahyu";
-        // mhs [0][1] = "1234567889";
-        // mhs [0][2] = "1c";
+        String back = "";
+        int n = 0, j, c;
+
         String mhs[][] = new String[1][3];
         String bio[] = { "Nama", "NIM", "kelas" };
-        String mhsn[][] = { { "Wahyu", "1234455", "1C" }, { "Rizky", "1122334", "1C" } };
+        String mhsn[][] = { { "Wahyu", "1234455", "1C" }, { "Rizky", "1122334", "1C" }, { "Cahyana", "1234466", "1C" },
+                { "Dizky", "1177334", "1C" } };
         String ab[] = new String[3];
-
         do {
             System.out.println("Bayak siswa");
             int d = scInt.nextInt();
@@ -30,51 +28,41 @@ public class main2 {
                     mhs[x][w] = "__";
                 }
             }
-
             for (int r = 0; r < mhsn.length; r++) {
                 for (int l = 0; l < bio.length; l++) {
                     mhs[r][l] = mhsn[r][l];
                 }
             }
             do {
-                // for (int a = 0; a < mhs.length; a++) {
-                // System.out.println(a + 1 + ". " + mhs[a][0]);
-                // }
                 for (int a = 0; a < mhs.length; a++) {
                     for (int b = 0; b < mhs.length; b++) {
-                        if (mhs[a][0].charAt(0) < mhs[b][0].charAt(0)) {
-
-                            for (int v = 0; v < bio.length; v++) {
-                                ab[v] = mhs[a][v];
-                                mhs[a][v] = mhs[b][v];
-                                mhs[b][v] = ab[v];
-                            }
-                        } else if (mhs[a][0].charAt(0) == mhs[b][0].charAt(0)) {
-                            if (mhs[a][0].charAt(1) < mhs[b][0].charAt(1)) {
+                            if (mhs[a][0].compareTo(mhs[b][0]) < 0) {
 
                                 for (int v = 0; v < bio.length; v++) {
                                     ab[v] = mhs[a][v];
                                     mhs[a][v] = mhs[b][v];
                                     mhs[b][v] = ab[v];
                                 }
-
-                            }
                         }
                     }
                 }
                 for (int s = 0; s < mhs.length; s++) {
-                    System.out.printf("%d.\t: %s\n",s + 1, mhs[s][0]);
+                    System.out.printf("%d.\t: %s\n", s + 1, mhs[s][0]);
                 }
-
                 System.out.println("Masukkan angka");
                 n = scInt.nextInt();
                 n -= 1;
-
                 for (j = 0; j < bio.length; j++) {
                     System.out.printf("Masukkan %s : ", bio[j]);
                     mhs[n][j] = scStr.nextLine();
                 }
-                System.out.println(mhs[0][1]);
+                for (int q = 0 ; q < mhs.length; q++){
+                    System.out.printf("%-3d|.\t|", q+1);
+                    for(int t = 0 ; t < bio.length; t++){
+                        System.out.printf(" %-6s|%-10s|", bio[t], mhs[q][t]);
+                    }
+                    System.out.println("");
+                }
 
                 System.out.println("ketik y");
                 back = scStr.nextLine();
