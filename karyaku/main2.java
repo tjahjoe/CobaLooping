d31a16aef0c3d05ca3ff0dece937324d34c67a07
tjahjoe <ntell634@gmail.com>
2023-10-14 05:27:02 +0700

    masih harus dikembangkan

--- karyaku/main2.java
+++ karyaku/main2.java
@@ -13,31 +13,44 @@ public class main2 {
 
 //menyesuaikan nilai
         String bio[] = { "Nama", "NIM", "kelas" };
+        String nilaiKul[] = {"Tugas", "Kuiz", "UTS", "UAS"};
         String mhsn[][] = { { "Wahyu", "1234455", "1C" }, { "Rizky", "1122334", "1C" }, { "Cahyana", "1234466", "1C" },
                 { "Dizky", "1177334", "1C" } };
-        int nilai[] = { 100, 98, 100, 89, 100, 0, 0, 0, 0, 0, 0, 0 };
+        int nilai[] = { 100, 98, 100, 89};
         String ab[] = new String[3];
         do {
             System.out.println("Bayak siswa");
             int d = scInt.nextInt();
             String aa[][] = new String[d][3];
             String mhs[][] = new String[d][3];
+            int nil[] = new int[d];
+            int nila[] =  new int[d];
 
             System.out.println(mhs.length);
             for (int x = 0; x < mhs.length; x++) {
+                    nil[x] = 0;
+                    nila[x] = 0;
                 for (int w = 0; w < bio.length; w++) {
                     mhs[x][w] = "__";
+                    aa[x][w] = "__";
+                    
                 }
             }
             for (int r = 0; r < mhsn.length; r++) {
                 for (int l = 0; l < bio.length; l++) {
                     aa[r][l] = mhsn[r][l];
-                }
-            }for (int r = 0; r < mhsn.length; r++) {
-                for (int l = 0; l < bio.length; l++) {
                     mhs[r][l] = aa[r][l];
                 }
-            }
+                
+            }for (int k = 0; k < nilai.length; k++){
+                    nil[k] = nilai[k];
+                    nila[k] = nil[k];
+                }
+            // }for (int r = 0; r < mhsn.length; r++) {
+            //     for (int l = 0; l < bio.length; l++) {
+            //         mhs[r][l] = aa[r][l];
+            //     }
+            // }
             do {
                 for (int s = 0; s < mhs.length; s++) {
                     System.out.printf("%d.\t: %s\n", s + 1, mhs[s][0]);
@@ -50,24 +63,27 @@ public class main2 {
                     aa[n][j] = scStr.nextLine();
                     mhs[n][j] = aa[n][j];
                 }
+                System.out.print("Masukkan nilai : ");
+                    nil[n] = scInt.nextInt();
+                    nila[n] = nil[n]; 
                 System.out.println(aa[4][0]);
                 for (int a = 0; a < mhs.length; a++) {
                     for (int b = 0; b < mhs.length; b++) {
-                        if (nilai[a] > nilai[b]) {
-                            c = nilai[a];
-                            nilai[a] = nilai[b];
-                            nilai[b] = c;
+                        if (nila[a] > nila[b]) {
+                            c = nila[a];
+                            nila[a] = nila[b];
+                            nila[b] = c;
 
                             for (int v = 0; v < bio.length; v++) {
                                 ab[v] = mhs[a][v];
                                 mhs[a][v] = mhs[b][v];
                                 mhs[b][v] = ab[v];
                             }
-                        } else if (nilai[a] == nilai[b]) {
+                        } else if (nila[a] == nila[b]) {
                             if (mhs[a][0].compareTo(mhs[b][0]) < 0) {
-                                c = nilai[a];
-                                nilai[a] = nilai[b];
-                                nilai[b] = c;
+                                c = nila[a];
+                                nila[a] = nila[b];
+                                nila[b] = c;
 
                                 for (int v = 0; v < bio.length; v++) {
                                     ab[v] = mhs[a][v];
@@ -84,7 +100,7 @@ public class main2 {
                     for (int t = 0; t < bio.length; t++) {
                         System.out.printf(" %-6s|%-10s|", bio[t], mhs[q][t]);
                     }
-                    System.out.printf("\t%d\n", nilai[q]);
+                    System.out.printf("\t%d\n", nila[q]);
                 }System.out.println(aa[4][0]);
                 for (int s = 0; s < mhs.length; s++) {
                     System.out.printf("%d.\t: %s\n", s + 1, aa[s][0]);
